1cdbbb0231208fb6c9584d90cb853ac795226492
Agustin Pelle <agustinpelle@hotmail.com>
2023-03-20 16:42:24 -0300

    Correciones

--- ProductManager.js
+++ ProductManager.js
@@ -8,29 +8,32 @@ class ProductManager {
     }
 
     addProduct(title, description, price, thumbnail, code, stock){
-        let id_producto = this.products.length;
+        let idProducto = this.products.length;
 
-        let producto = {
+        const producto = {
             title:title,
             description:description,
             price:price,
             thumbnail:thumbnail,
             code:code,
             stock:stock,
-            id:++id_producto
+            id:++idProducto
         }
-        if (producto.title == null ||producto.title  == ''){
+        if (producto.title === null ||producto.title  === ''){
             return console.log('Insert the title')
         }
-        if (producto.description == null ||producto.description  == ''){
+        if (producto.description === null ||producto.description  === ''){
             return console.log('Insert a description')
+        }
+        if (producto.code === null || producto.code === ''){
+            return console.log('Insert a code')
         }
         this.products.push(producto);
         return this.products;
     }
 
-    getProductsById(id_producto){   
-        let producto = this.products.find(producto=> products.id === id_producto)
+    getProductById(idProducto){   
+        let producto = this.products.find(producto=> products.id === idProducto)
 
         if(producto){
             return producto;
