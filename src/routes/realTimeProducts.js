8fbe46129b47fc864884fb18a72293b54264462b
Agustin Pelle <agustinpelle@hotmail.com>
2023-04-20 00:13:53 -0300

    commit

--- src/routes/realTimeProducts.js
+++ src/routes/realTimeProducts.js
@@ -10,13 +10,13 @@ const router = Router();
 const io = new Server()
 
 router.get('/', async (req, res) => {
-    // Obtener los productos usando el método getProducts de ProductManager
+   
     const products = await productManager.getProducts();
     
-    // Renderizar la vista "realTimeProducts.handlebars\" con los productos obtenidos
+   
     res.render('realTimeProducts', {products});
   
-    // Enviar los datos en tiempo real a la vista usando socket.io
+    
     io.on('connection', (socket) => {
       socket.emit('products', products);
     });
@@ -25,13 +25,12 @@ router.get('/', async (req, res) => {
   router.post('/', async (req, res) => {
     const { title, description, price, thumbnail, code, stock } = req.body;
     
-    // Agregar el nuevo producto usando el método addProducts de ProductManager
+
     const newProduct = await productManager.addProducts({ title, description, price, thumbnail, code, stock });
     
-    // Enviar los datos del nuevo producto en tiempo real a la vista usando socket.io
+   
     io.emit('products', newProduct);
     
-    // Redireccionar a la página principal de productos
     res.redirect('/');
   });
 
