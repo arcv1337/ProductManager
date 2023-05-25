0ce43491e17a79380c9dbeca50f9e99c27667d67
Agustin Pelle <agustinpelle@hotmail.com>
2023-05-25 14:18:30 -0300

    falta log

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,15 +1,47 @@
-import { Router } from "express";
-import express from 'express';
-import ProductManager from '../Dao/managers/productsManager.js';
+import { Router } from 'express';
+import path from 'path'
+import exphbs from 'express-handlebars';
+import __dirname from '../utils.js';
+import ProductManagerMongo from '../Dao/managers/productsManagerMongo.js';
 
-const productManager = new ProductManager()
-
+const productManagerMongo = new ProductManagerMongo();
 const router = Router();
 
-router.get('/', async (req,res)=>{
-    const products = await productManager.getProducts();
-    res.render('home', { products });
+// Configuración del motor de vistas Handlebars
+const viewsDir = path.join(__dirname, '..', 'views');
+const layoutsDir = path.join(viewsDir, 'layouts');
+const partialsDir = path.join(viewsDir, 'partials');
+
+const handlebars = exphbs.create({
+  defaultLayout: 'main',
+  layoutsDir,
+  partialsDir,
 });
 
+router.get('/products', async (req, res) => {
+  try {
+    const queryParams = {
+      limit: req.query.limit,
+      page: req.query.page,
+      sort: req.query.sort,
+      query: req.query.query,
+    };
+
+    const response = await productManagerMongo.getProducts(queryParams);
+
+    res.render('products', {
+      products: response.payload,
+      hasPrevPage: response.hasPrevPage,
+      hasNextPage: response.hasNextPage,
+      prevLink: response.prevLink,
+      nextLink: response.nextLink,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+});
 
 export default router;
